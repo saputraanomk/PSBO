21f70991f4af271cdfbbea670dbb673f2f21c710
Sarah Al Qibthiyyah <sqibtya@yahoo.com>
2017-05-15 20:09:02 +0700

    plus login

--- LatihanLagi/app/src/main/java/com/example/alqibthiyyah/latihanlagi/Activities/MainActivity.java
+++ LatihanLagi/app/src/main/java/com/example/alqibthiyyah/latihanlagi/Activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alqibthiyyah.latihanlagi.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.alqibthiyyah.latihanlagi.Adapters.CobaAdapters;
@@ -9,15 +11,29 @@ import com.example.alqibthiyyah.latihanlagi.R;
 
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
+    //ListView listView;
+    EditText txtViewuname, txtViewpass;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CobaAdapters adapters = new CobaAdapters(MainActivity.this);
+        //CobaAdapters adapters = new CobaAdapters(MainActivity.this);
 
-        listView = (ListView)findViewById(R.id.ListView);
-        listView.setAdapter(adapters);
+       // listView = (ListView)findViewById(R.id.ListView);
+        //listView.setAdapter(adapters);
+
+        txtViewuname = (EditText)findViewById(R.id.txtViewuname);
+        txtViewpass = (EditText)findViewById(R.id.txtViewpass);
+    }
+
+    public void OnLogin (View view){
+        String username = txtViewuname.getText().toString();
+        String password = txtViewpass.getText().toString();
+        String type = "login";
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+
     }
 }
